7ce240e4526786cb19d785714957e2a785cb3a51
getdagnis <dagnis.skurbe@gmail.com>
2020-06-22 19:12:12 +0300

    Updates

---
+++ precache-manifest.3f902378b81a276e45812fd8e19f7ff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f111927c5ab43efa3090b444d7baf8",
+    "url": "/react-dark-event/index.html"
+  },
+  {
+    "revision": "ed7af4698137ec945e78",
+    "url": "/react-dark-event/static/css/main.21ae6630.chunk.css"
+  },
+  {
+    "revision": "67c21e0d7ddc54c9c481",
+    "url": "/react-dark-event/static/js/2.01690b57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-dark-event/static/js/2.01690b57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7af4698137ec945e78",
+    "url": "/react-dark-event/static/js/main.26859618.chunk.js"
+  },
+  {
+    "revision": "5c9b5ea9703a9b327c63",
+    "url": "/react-dark-event/static/js/runtime-main.a7931bf9.js"
+  }
+]);
